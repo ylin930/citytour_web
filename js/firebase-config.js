9383f8ca9b59798b185170f39b248807119aca02
Ying Lin <yinglin930@gmail.com>
2025-10-14 22:13:30 +0200

    initial commit

---
+++ js/firebase-config.js
@@ -0,0 +1,9 @@
+// Your web app's Firebase configuration
+export const firebaseConfig = {
+  apiKey: "AIzaSyD0rObwGbmpbbXzpkZmdv1jwmTTUhRmwjY",
+  authDomain: "citytour-67d32.firebaseapp.com",
+  projectId: "citytour-67d32",
+  storageBucket: "citytour-67d32.firebasestorage.app",
+  messagingSenderId: "84870954433",
+  appId: "1:84870954433:web:68a9ed5f8d9b3c15040340"
+};
