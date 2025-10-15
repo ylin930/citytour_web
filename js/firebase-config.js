91a0d9bbfb5af117884dc8213034abd6ff8ba9c2
Ying Lin <yinglin930@gmail.com>
2025-10-15 16:32:25 +0200

    updating and working on cover story html

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -4,8 +4,8 @@ window.FB_CONFIG = {
   apiKey: "AIzaSyD0rObwGbmpbbXzpkZmdv1jwmTTUhRmwjY",
   authDomain: "citytour-67d32.firebaseapp.com",
   projectId: "citytour-67d32",
-  // If you use Storage, uncomment and use the standard bucket:
-  // storageBucket: "citytour-67d32.appspot.com",
+  storageBucket: "citytour-67d32.firebasestorage.app",
   messagingSenderId: "84870954433",
   appId: "1:84870954433:web:68a9ed5f8d9b3c15040340"
 };
+
